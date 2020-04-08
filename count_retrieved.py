41edb3e2feb41349cfe45c8a847298db178fb2c7
UmutAlihan <alihandikel@gmail.com>
2020-04-09 00:49:20 +0300

    modify count_retrieved

--- count_retrieved.py
+++ count_retrieved.py
@@ -5,13 +5,19 @@ import dateutil.relativedelta
 import datetime
 from time import time
 import csv
+import logging
 
 service = gb.auth_service()
 mailIds = gb.list_all_messages(service, "me")
+logging.info("retrieving mails")
 mailBox = gb.mailBox_retriever(service, mailIds, verbose=True)
 
+logging.info("counting mails")
 counted_messages, sorted_counted_messages = gb.count_retrieved_messages(mailBox)
 
-w = csv.writer(open("counted_messages.csv", "w"))
+
+filename = "counted_messages.csv"
+logging.info("writing into: " + filename)
+w = csv.writer(open(filename, "w"))
 for key, val in counted_messages.items():
     w.writerow([key, val])
