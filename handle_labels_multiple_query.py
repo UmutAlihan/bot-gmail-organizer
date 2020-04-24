05935e9eae9cf41d8b2123df0a3a9a547c16370d
UmutAlihan <alihandikel@gmail.com>
2020-04-24 17:34:27 +0300

    add TODO

--- handle_labels_multiple_query.py
+++ handle_labels_multiple_query.py
@@ -16,8 +16,8 @@ def execute(args):
             sys.exit()
 
 
-        logging.info("INIT"); time.sleep(2)        
-        #parse arguments        
+        logging.info("INIT"); time.sleep(2)
+        #parse arguments
         account = args[0] # account name to login
         queries = args[1:-1] #exclude label & accountname, only query kwords
         labelname = args[-1] # include only label
